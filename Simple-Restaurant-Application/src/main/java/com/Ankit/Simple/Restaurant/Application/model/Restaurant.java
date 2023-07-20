7710071a0e5364f0fc8c2cc2adc5871fd35c5c29
Ankit kumar <anktikumar@gnail.com>
2023-07-20 16:05:46 +0530

    add @Pattern annotation

--- Simple-Restaurant-Application/src/main/java/com/Ankit/Simple/Restaurant/Application/model/Restaurant.java
+++ Simple-Restaurant-Application/src/main/java/com/Ankit/Simple/Restaurant/Application/model/Restaurant.java
@@ -1,8 +1,10 @@
 package com.Ankit.Simple.Restaurant.Application.model;
 
 import com.Ankit.Simple.Restaurant.Application.model.categeory.Categeory;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +20,9 @@ public class Restaurant {
     private Integer restaurantId;
     @NotBlank
     private String restaurantName;
+    @Email
     private String restaurantAddress;
+    @Pattern(regexp = "^91\\d{10}$")
     private String restaurantPhone;
     private Categeory categeory;
     @Autowired
